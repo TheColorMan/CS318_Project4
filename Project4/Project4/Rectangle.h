c5f003315279c6c835bd1fd006ce347450350abe
Kevin Prehn <kprehn01@gmail.com>
2016-11-04 13:12:02 -0500

    Initial commit. Added all given project files.

---
+++ Project4/Project4/Rectangle.h
@@ -0,0 +1,15 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+class Rectangle // : extending something
+{
+	// default constructor with parameters: length, width, x, y
+	Rectangle(double = 0.0, double = 0.0, double = 0.0, double = 0.0);
+	virtual double getLength() const; // return length of Rectangle
+	virtual double getWidth() const; // return the width of Rectangle
+	virtual double getArea() const; // return area of Rectangle
+	void print() const; // output Rectangle object
+
+};
+
+#endif
